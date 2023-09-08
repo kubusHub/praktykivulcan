8b471a6342f84d106625da3875f96c0eb888d71f
Szymon Gregorczuk <szymongrego08@gmail.com>
2023-09-08 14:27:22 +0200

    silnie dzialaja

--- SzymonGregorczuk/rekurencja/silnie rekurencyjne/script.js
+++ SzymonGregorczuk/rekurencja/silnie rekurencyjne/script.js
@@ -1,19 +1,21 @@
 let button = document.querySelectorAll("input[type=button]")
-let nr = document.getElementById("numer")
-nr = Number(nr.value)
-console.log(nr.value)
 let output = document.querySelector("h1")
-function silnia(n){
-    if(n<0){
-        return
-    }
-    else{
-        let wynik = n * silnia(n-1)
+function silnia(x){
+    if(x>=0){
+
+    
+        if (x == 0) {
+            return 1;
+        }
+        else {
+            return x * silnia(x - 1);
+        }
     }
 }
 for (const b of button) {
     b.addEventListener("click", function(){
-        silnia(nr.value)
-        console.log(wynik)
+        let nr = document.getElementById("numer").value
+        nr = +nr
+        console.log(silnia(nr))
     })
 }
